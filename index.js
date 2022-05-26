b9ff286f3f5a3f18f93ee571aa7db4c70a13f753
Ryan Barrera <rbarrera9er@gmail.com>
2022-05-25 21:52:05 -0400

    commit tree 29f1824fd73feb5d44867e0d65fddfe12359a1eb

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+   const allCats = [...cats];
+   allCats.push(name);
+   return allCats
+}
+function prependCat(name){
+    const allCats = [...cats];
+    allCats.unshift(name);
+    return allCats;
+}
+function removeLastCat(){
+    const allcats = [...cats];
+    allcats.pop();
+    return allcats
+}
+function removeFirstCat(){
+    const allcats = [...cats];
+    allcats.shift();
+    return allcats
+}
